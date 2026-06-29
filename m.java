abde1cd9c475fc01a6daddfc35fc5cd04c4f1719
Leopold Allen <157778331+CodeNinja369@users.noreply.github.com>
2026-06-29 10:10:26 +1000

    testing

--- m.java
+++ m.java
@@ -1,11 +1,11 @@
 import java.awt.image.BufferedImage;
 public class m{
     public static void main(String[]args){
-        int w = 205;
-        int h = 246;
+        int w = 643;
+        int h = 360;
         BufferedImage demoIm = null;
         BufferedImage mapIm = null;
-        processor demo = new processor("demo1.jpg", "demo1Out3.jpg", w, h, demoIm);
+        processor demo = new processor("demo2.jpg", "demo2Out3.jpg", w, h, demoIm);
 
         demo.read();
 
