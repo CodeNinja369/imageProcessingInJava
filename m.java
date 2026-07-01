f68655d6c70d0af22ae9bd83b26136d695846dfb
Leopold Allen <157778331+CodeNinja369@users.noreply.github.com>
2026-07-01 11:37:39 +1000

    compound filters implemented

--- m.java
+++ m.java
@@ -1,16 +1,55 @@
 import java.awt.image.BufferedImage;
+import java.util.Scanner;
 public class m{
     public static void main(String[]args){
-        int w = 643;
-        int h = 360;
+        int w = 205;
+        int h = 246;
         BufferedImage demoIm = null;
         BufferedImage mapIm = null;
-        processor demo = new processor("demo2.jpg", "demo2Out3.jpg", w, h, demoIm);
+        String inp = "";
+        Scanner scnr = new Scanner(System.in);
+        processor demo = new processor("io.jpg", "io.jpg", w, h, demoIm);
+        while(inp!="e"){
+            System.out.print("input number:");
+            inp = scnr.nextLine();
+            if(!(inp.equals(""))){
+                demo.read();
+                if(inp.equals("s1")){
+                    demo.sort1();
+                }
+                if(inp.equals("s2")){
+                    demo.sort2();
+                }
+                if(inp.equals("s3")){
+                    demo.sort3();
+                }
+                if(inp.equals("-r")){
+                    demo.noRed();
+                }
+                if(inp.equals("-g")){
+                    demo.noGreen();
+                }
+                if(inp.equals("-b")){
+                    demo.noBlue();
+                }
+                if(inp.equals("-gb")){
+                    demo.onlyRed();
+                }
+                if(inp.equals("-rb")){
+                    demo.onlyGreen();
+                }
+                if(inp.equals("-rg")){
+                    demo.onlyBlue();
+                }
+                if(inp.equals("a1")){
+                    
+                }
 
-        demo.read();
-
-        demo.sort3();
-        demo.write();
+                demo.write();
+            }
+            
+        }
+        
         
     }
     
