f7d7718bf250d000e02f59297e9501c232755a86
Leopold Allen <157778331+CodeNinja369@users.noreply.github.com>
2026-07-02 20:33:17 +1000

    major overhaul

--- processor.java
+++ processor.java
@@ -10,8 +10,8 @@ public class processor {
     private int xSize;
     private int ySize;
     private BufferedImage image;
-    private pixel[] pArray;
-    private int[] fArray;
+    private pixel[][] pArray;
+    private int[] cArray;
     public processor(String fname, String fOut, int x,int y, BufferedImage e){
         this.fileName = fname;
         this.fileOutName = fOut;
@@ -19,8 +19,8 @@ public class processor {
         this.ySize = y;
         this.image = e;
         read();
-        this.fArray = getFarray();
-        this.pArray = getPArray();
+        this.cArray = getCarray();
+        this.pArray = getParray();
     }
     //read and write to image files
     public void read() {
@@ -41,17 +41,19 @@ public class processor {
         }
     }
     //create array of pixel objects in an image
-    private pixel[] getPArray(){
+    private pixel[][] getParray(){
         int d = 0;
-        pixel[] out = new pixel[this.xSize*this.ySize];
-        for(int i = 0; i<this.fArray.length; i++){
-            out[i] = new pixel(this.fArray[i]);
+        pixel[][] out = new pixel[this.xSize][this.ySize];
+        for(int x =0; x<this.xSize; x++){
+            for(int y = 0; y<this.ySize; y++){
+                out[x][y] = new pixel(this.image.getRGB(x, y), x, y);
+                d++;
+            }
         }
-        
         return out;
     }
     //created flattened array
-    private int [] getFarray(){
+    private int [] getCarray(){
         int d = 0;
         int[] out = new int[this.xSize*this.ySize];
         for(int x =0; x<this.xSize; x++){
@@ -66,10 +68,10 @@ public class processor {
     private int partition(Field f, int low, int high) {
         int i = low - 1;
         try {
-            double pivot = Double.parseDouble(f.get(pArray[high]).toString());
+            double pivot = Double.parseDouble(f.get(getPixel(high)).toString());
 
             for (int j = low; j <= high - 1; j++) {
-                if (Double.parseDouble(f.get(pArray[j]).toString()) < pivot) {
+                if (Double.parseDouble(f.get(getPixel(j)).toString()) < pivot) {
                     i++;
                     swap(i, j);
                 }
@@ -82,10 +84,23 @@ public class processor {
         return i + 1;
     }
 
+    // translate a flat index into the 2D array
+    private pixel getPixel(int index) {
+        int x = index % xSize;
+        int y = index / xSize;
+        return pArray[x][y];
+    }
+
+    private void setPixel(int index, pixel p) {
+        int x = index % xSize;
+        int y = index / xSize;
+        pArray[x][y] = p;
+    }
+
     private void swap(int i, int j) {
-        pixel temp = this.pArray[i];
-        pArray[i] = pArray[j];
-        pArray[j] = temp;
+        pixel temp = getPixel(i);
+        setPixel(i, getPixel(j));
+        setPixel(j, temp);
     }
 
     public void quickSort(String att, int low, int high) {
@@ -106,187 +121,217 @@ public class processor {
     }
     //update image file
     private void update(){
-        int e = 0;
         for (int x = 0; x < this.xSize; x++) {
             for (int y = 0; y < this.ySize; y++) {
-                this.image.setRGB(x, y, this.pArray[e].pixelC);
-                e++;
+                this.image.setRGB(x, y, this.pArray[x][y].pixelC);
+
             }
         }
     }
     //sorts base 255 colour values using quicksort
     public void sort1() {
-        quickSort("pixelC", 0, pArray.length - 1);
+        quickSort("pixelC", 0, xSize*ySize - 1);
         update();
     }
     //sorting by brightness calculated for differences in human vision
     public void sort2(){
-        quickSort("pixelB", 0, this.pArray.length-1);
+        quickSort("pixelB", 0, xSize*ySize-1);
         update();
     }
 
 
 //sorted by red value
     public void sort2_2(){
-        quickSort("red", 0, this.pArray.length-1);
+        quickSort("red", 0, xSize*ySize-1);
         update();
     }
     //sorted by green
     public void sort2_3(){
-        quickSort("green", 0, this.pArray.length-1);
+        quickSort("green", 0, xSize*ySize-1);
         update();
     }
     //sorted by blue
     public void sort2_4(){
-        quickSort("blue", 0, this.pArray.length-1);
+        quickSort("blue", 0, xSize*ySize-1);
         update();
     }
 
     //sorts into checkerboard-like pattern of alternating pixels with maximised contrast
     public void sort3(){
-        for(int index = 0; index < pArray.length - 1; index++){
-            pixel c1 = pArray[index];
+        int len = xSize * ySize;
+        for(int index = 0; index < len - 1; index++){
+            pixel c1 = pArray[index % xSize][index / xSize];
             pixel pstore = c1;
             int ppos = 0; 
             double contstore = 0;
-            for(int i = index+1; i < pArray.length; i++){
+            for(int i = index+1; i < len; i++){
                 double contrast = 0;
-                if(c1.pixelB > pArray[i].pixelB){
-                    contrast = (c1.pixelB + 0.05) / (pArray[i].pixelB + 0.05);
+                if(c1.pixelB > pArray[i % xSize][i / xSize].pixelB){
+                    contrast = (c1.pixelB + 0.05) / (pArray[i % xSize][i / xSize].pixelB + 0.05);
                 }
-                if(pArray[i].pixelB > c1.pixelB){
-                    contrast = (pArray[i].pixelB + 0.05) / (c1.pixelB + 0.05);
+                if(pArray[i % xSize][i / xSize].pixelB > c1.pixelB){
+                    contrast = (pArray[i % xSize][i / xSize].pixelB + 0.05) / (c1.pixelB + 0.05);
                 }
                 if(contstore < contrast){
                     contstore = contrast;
-                    pstore = pArray[i];
+                    pstore = pArray[i % xSize][i / xSize];
                     ppos = i;
                 }
             }
-            pixel np = pArray[index+1];
-            pArray[index+1] = pstore;
-            pArray[ppos] = np;
+            pixel np = pArray[(index+1) % xSize][(index+1) / xSize];
+            pArray[(index+1) % xSize][(index+1) / xSize] = pstore;
+            pArray[ppos % xSize][ppos / xSize] = np;
         }
         int e = 0;
         update();
     }
     //creates brightness map of image
     public int[] createMap() {
-        int[] map = new int[pArray.length];
-    
-        for (int i = 0; i < pArray.length; i++) {
+        int rows = pArray.length;
+        int cols = pArray[0].length;
+        int total = rows * cols;
+        int[] map = new int[total];
+
+        for (int i = 0; i < total; i++) {
+            int ri = i / cols, ci = i % cols;
             int rank = 0;
-            for (int j = 0; j < pArray.length-1; j++) {
-                if (pArray[j].pixelB <= pArray[i].pixelB) {
+            for (int j = 0; j < total - 1; j++) {
+                int rj = j / cols, cj = j % cols;
+                if (pArray[rj][cj].pixelB <= pArray[ri][ci].pixelB) {
                     rank++;
                 }
             }
             map[i] = rank;
         }
-    
+
         return map;
-    }
+}
+
     //recolours image using brightness map, and image of equal size sorted by brightness
-    public void obamaAlg(processor pp){
+    public void obamaAlg(processor pp) {
         int[] m = createMap();
-        for(int i =0; i<pArray.length-1; i++){
-            this.pArray[i] = pp.pArray[m[i]];
+        int rows = pArray.length;
+        int cols = pArray[0].length;
+        int total = rows * cols;
+
+        for (int i = 0; i < total - 1; i++) {
+            int ri = i / cols, ci = i % cols;
+            int rank = m[i];
+            int rr = rank / cols, rc = rank % cols;
+            this.pArray[ri][ci] = pp.pArray[rr][rc];
         }
         update();
     }
     //remove Red, Green or Blue, or any combination of two
     public void onlyRed(){
-        for(int i =0; i<pArray.length; i++){
-            pArray[i].green=0;
-            pArray[i].blue=0;
-            pArray[i].updateC();
+        for(int i =0; i<this.xSize; i++){
+            for(int j =0; j<this.ySize; j++){
+                pArray[i][j].green=0;
+                pArray[i][j].blue=0;
+                pArray[i][j].updateC();
+            }
         }
         update();
     }
     public void onlyBlue(){
         for(int i =0; i<pArray.length; i++){
-            pArray[i].red=0;
-            pArray[i].blue=0;
-            pArray[i].updateC();
+            for(int j =0; j<this.ySize; j++){
+                pArray[i][j].red=0;
+                pArray[i][j].green=0;
+                pArray[i][j].updateC();
+            }
         }
         update();
     }
     public void onlyGreen(){
         for(int i =0; i<pArray.length; i++){
-            pArray[i].red=0;
-            pArray[i].blue=0;
-            pArray[i].updateC();
+            for(int j =0; j<this.ySize; j++){
+                pArray[i][j].red=0;
+                pArray[i][j].blue=0;
+                pArray[i][j].updateC();
+            }
         }
         update();
     }
     public void noRed(){
         for(int i =0; i<pArray.length; i++){
-            pArray[i].red=0;
-            pArray[i].updateC();
+            for(int j =0; j<this.ySize; j++){
+                pArray[i][j].green=0;
+                pArray[i][j].blue=0;
+                pArray[i][j].updateC();
+            }
         }
         update();
     }
     public void noGreen(){
         for(int i =0; i<pArray.length; i++){
-            pArray[i].green=0;
-            pArray[i].updateC();
+            for(int j =0; j<this.ySize; j++){
+                pArray[i][j].green=0;
+                pArray[i][j].updateC();
+            }
         }
         update();
     }
     public void noBlue(){
         for(int i =0; i<pArray.length; i++){
-            pArray[i].blue=0;
-            pArray[i].updateC();
+            for(int j =0; j<this.ySize; j++){
+                pArray[i][j].blue=0;
+                pArray[i][j].updateC();
+            }
         }
         update();
     }
     public void noWhite1(){
-        for(int i = 0; i < pArray.length; i++){
-            int min = Math.min(pArray[i].red, Math.min(pArray[i].green, pArray[i].blue));
-            if(min>0){
-                pArray[i].red   = pArray[i].red%min;
-                pArray[i].green = pArray[i].green%min;
-                pArray[i].blue  = pArray[i].blue%min;
-                
+        for(int i = 0; i < xSize; i++){
+            for(int j = 0; j<ySize; j++){
+                int min = Math.min(pArray[i][j].red, Math.min(pArray[i][j].green, pArray[i][j].blue));
+                if(min>0){
+                    pArray[i][j].red   = pArray[i][j].red%min;
+                    pArray[i][j].green = pArray[i][j].green%min;
+                    pArray[i][j].blue  = pArray[i][j].blue%min;
+                    
+                }
+                pArray[i][j].updateC();
             }
-            pArray[i].updateC();
-            
         }
         update();
     }
     public void noWhite2(){
-        for(int i = 0; i < pArray.length; i++){
-            int min = Math.min(pArray[i].red, Math.min(pArray[i].green, pArray[i].blue));
-            if(min>0){
-                pArray[i].red   = pArray[i].red-min;
-                pArray[i].green = pArray[i].green-min;
-                pArray[i].blue  = pArray[i].blue-min;
-                
+        for(int i = 0; i < xSize; i++){
+            for(int j = 0; j<ySize; j++){
+                int min = Math.min(pArray[i][j].red, Math.min(pArray[i][j].green, pArray[i][j].blue));
+                if(min>0){
+                    pArray[i][j].red   = pArray[i][j].red-min;
+                    pArray[i][j].green = pArray[i][j].green-min;
+                    pArray[i][j].blue  = pArray[i][j].blue-min;
+                    
+                }
+                pArray[i][j].updateC();
             }
-            pArray[i].updateC();
-            
         }
         update();
     }
     public void white(){
-        for(int i = 0; i < pArray.length; i++){
-            int max = Math.max(pArray[i].red, Math.max(pArray[i].green, pArray[i].blue));
-            pArray[i].red   = max;
-            pArray[i].green = max;
-            pArray[i].blue  = max;
-            pArray[i].updateC();
-            
+        for(int i = 0; i < xSize; i++){
+            for(int j = 0; j<ySize; j++){
+                int max = Math.max(pArray[i][j].red, Math.max(pArray[i][j].green, pArray[i][j].blue));
+                pArray[i][j].red   = max;
+                pArray[i][j].green = max;
+                pArray[i][j].blue  = max;
+                pArray[i][j].updateC();
+            }
         }
         update();
     }
     public void black(){
-        for(int i = 0; i < pArray.length; i++){
-            int max = Math.min(pArray[i].red, Math.min(pArray[i].green, pArray[i].blue));
-            pArray[i].red   = max;
-            pArray[i].green = max;
-            pArray[i].blue  = max;
-            pArray[i].updateC();
-            
+        for(int i = 0; i < xSize; i++){
+            for(int j = 0; j<ySize; j++){
+                int min = Math.min(pArray[i][j].red, Math.min(pArray[i][j].green, pArray[i][j].blue));
+                pArray[i][j].red   = min;
+                pArray[i][j].green = min;
+                pArray[i][j].blue  = min;
+                pArray[i][j].updateC();
+            }
         }
         update();
     }
@@ -295,29 +340,30 @@ public class processor {
     public void average1(){
         int[] rowAv = new int[3];
         int[][] rAvs = new int[this.ySize][3];
-        int e =0;
+
         for(int i =0; i<this.ySize; i++){
             for(int j = 0; j<this.xSize; j++){
-                rowAv[0]+=pArray[e].red;
-                rowAv[1]+=pArray[e].green;
-                rowAv[2]+=pArray[e].blue;
-                e++;
+                rowAv[0]+=pArray[i][j].red;
+                rowAv[1]+=pArray[i][j].green;
+                rowAv[2]+=pArray[i][j].blue;
+           
             }
             rAvs[i][0] = rowAv[0]/this.xSize;
             rAvs[i][1] = rowAv[1]/this.xSize;
             rAvs[i][2] = rowAv[2]/this.xSize;
             rowAv = new int[3];
         }
-        e=0;
+  
         for(int i =0; i<this.ySize; i++){
             for(int j = 0; j<this.xSize; j++){
-                pArray[e].red = rAvs[i][0];
-                pArray[e].green = rAvs[i][1];
-                pArray[e].blue = rAvs[i][2];
-                pArray[e].updateC();
-                e++;
+                pArray[i][j].red = rAvs[i][0];
+                pArray[i][j].green = rAvs[i][1];
+                pArray[i][j].blue = rAvs[i][2];
+                pArray[i][j].updateC();
+
             }
         }
         update();
     }
+   
 }
